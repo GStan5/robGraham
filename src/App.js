aa7d451e5c413823768e233ba7266472c8684881
Gavin <gavinstanifer@live.com>
2022-04-19 18:23:34 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 const axios = require("axios");
 const { wordsToNumbers } = require('words-to-numbers');
 
 
 let dataOne = 0;
 let dataTwo = 0;
+let plusAnswer = 0;
+let minusAnswer = 0;
+let timesAnswer = 0;
+let dividedAnswer = 0;
 let operations = ["plus", "minus", "times", "divided by"];
 
-async function onClick(){
-  console.log("clicked");
-  let data;
-  var config = {
-    method: 'get',
-    url: "https://100insure.com/mi/api1.php"
-  };
-
-  await axios(config)
-  .then(function (response) {
-      console.log (response.data);
-      data = response.data;
-      dataOne = wordsToNumbers(data.key1);
-      dataTwo = wordsToNumbers(data.key2);
-  })
-  .catch(function (error) {
-      console.log(error);
-  });
-
-  App();
-
-/*
-  var configTwo = {
-    method: 'post',
-    url: "https://100insure.com/mi/api2.php",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: {
-      num1: dataOne,
-      num2: dataTwo,
-      operation: plus
-    }
-  }
-
-  console.log(configTwo.data)
-  await axios (configTwo)
-  .then(function (response) {
-      console.log (response.data);
-      data = response.data;
-  })
-  .catch(function (error) {
-      console.log(error);
-  });
-*/
-}
-
-class firstNumber extends React.Component{
-  render() {
-    return (
-      <button className="numberOne">
-        {dataOne}
-      </button>
-    );
-  }
-}
 
 
 function App() {
 
+  //Style
   const styleObj = {
     fontSize: 44,
     color: "white",
@@ -75,80 +24,135 @@ function App() {
     paddingTop: "100px",
   }
 
-  const [posts, setPosts] = useState([]);
-
+  //URIs
   const apiEndPointOne = "https://100insure.com/mi/api1.php";
+  const apiEndPointtwo = 'https://100insure.com/mi/api2.php'
 
-  let data = {
-    num1: dataOne,
-    num2: dataTwo,
-    operation: "plus"
+
+  //usStates
+  const [valueOneData, setValueOne] = useState(0);
+  const [valueTwoData, setValueTwo] = useState(0);
+  const [plusAnswerData, setPlusAnswer] = useState(0);
+  const [minusAnswerData, setMinusAnswer] = useState(0);
+  const [timesAnswerData, setTimesAnswer] = useState(0);
+  const [dividedAnswerData, setDividedAnswer] = useState(0);
+
+
+  //gets all the updated values
+  const getValues = async () => {
+
+    //initial variables from apiEndPointOne
+    const resultOne = await axios.get(apiEndPointOne);
+    dataOne =  wordsToNumbers(resultOne.data.key1);
+    dataTwo =  wordsToNumbers(resultOne.data.key2);
+    setValueOne(dataOne);
+    setValueTwo(dataTwo);
+
+
+
+    //plus call
+    var configPlus = {
+      method: 'post',
+      url: apiEndPointtwo,
+      data : JSON.stringify({
+        "num1": dataOne,
+        "num2": dataTwo,
+        "operation": operations[0]
+      })
+    };
+    axios(configPlus)
+    .then(function (response) {
+      console.log("plus: " + response.data);
+      plusAnswer = response.data
+      setPlusAnswer(plusAnswer); 
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+
+    //minus call
+    var configMinus = {
+      method: 'post',
+      url: apiEndPointtwo,
+      data : JSON.stringify({
+        "num1": dataOne,
+        "num2": dataTwo,
+        "operation": operations[1]
+      })
+    };
+    axios(configMinus)
+    .then(function (response) {
+      console.log("minus: " + response.data);
+      minusAnswer = response.data
+      setMinusAnswer(minusAnswer); 
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+
+    //times call
+    var configTimes = {
+      method: 'post',
+      url: apiEndPointtwo,
+      data : JSON.stringify({
+        "num1": dataOne,
+        "num2": dataTwo,
+        "operation": operations[2]
+      })
+    };
+    axios(configTimes)
+    .then(function (response) {
+      console.log("times: " + response.data);
+      timesAnswer = response.data
+      setTimesAnswer(timesAnswer); 
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+
+    //divide call
+    var configDivide = {
+      method: 'post',
+      url: apiEndPointtwo,
+      data : JSON.stringify({
+        "num1": dataOne,
+        "num2": dataTwo,
+        "operation": operations[3]
+      })
+    };
+    axios(configDivide)
+    .then(function (response) {
+      console.log("divide: " + response.data);
+      dividedAnswer = response.data
+      setDividedAnswer(dividedAnswer); 
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
-  useEffect(() => {
-    const getPosts = async () => {
-      const resultOne = await axios.get(apiEndPointOne);
-      //console.log(resultOne.data)
-      dataOne =  wordsToNumbers(resultOne.data.key1);
-      dataTwo =  wordsToNumbers(resultOne.data.key2);
-      setPosts(resultOne);
-
-      let resultTwo;
-      var configTwo = {
-        method: 'post',
-        url: "https://100insure.com/mi/api2.php",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        num1: dataOne,
-        num2: dataTwo,
-        operation: "plus"  
-      }
-      resultTwo = await axios.post('https://100insure.com/mi/api2.php', { 
-        num1: dataOne,
-        num2: dataTwo,
-        operation: "plus"   
-      }, {
-        headers: {
-          // 'application/json' is the modern content-type for JSON, but some
-          // older servers may use 'text/json'.
-          // See: http://bit.ly/text-json
-          'Content-Type': 'application/json'
-        }
-      });
-      console.log(resultTwo);
-/*
-      await axios (configTwo)
-      .then(function (response) {
-        console.log (response);
-        console.log (response.data);
-        resultTwo = response.data;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-      console.log(resultTwo.data);
-*/
-    };
-
-    getPosts();
-
-    
-  }, []);
-  
-  
-  console.log(`B
+  /*
+  console.log(`DATA:
               ${dataOne}
               ${dataTwo}
               `);
+
+  */
+
   return (
     <div className="App">
-    <button onClick = {App}type="RUN">RUN</button>
+    <button onClick = {getValues}type="RUN">RUN</button>
     
-    <p style={styleObj}>{dataOne}+{dataTwo}=</p>
-    <p style={styleObj}>{dataOne}-{dataTwo}=</p>
-    <p style={styleObj}>{dataOne}*{dataTwo}=</p>
-    <p style={styleObj}>{dataOne}÷{dataTwo}=</p>
+    <p style={styleObj}>{dataOne}+{dataTwo}={plusAnswer}</p>
+    <p style={styleObj}>{dataOne}-{dataTwo}={minusAnswer}</p>
+    <p style={styleObj}>{dataOne}*{dataTwo}={timesAnswer}</p>
+    <p style={styleObj}>{dataOne}÷{dataTwo}={dividedAnswer}</p>
   
     </div>
   );
